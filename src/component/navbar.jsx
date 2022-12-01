661530da87a9ac7b4d99c957ebee462ea5c7ba0b
Abdullah Niaz <abdullahniaz39@gmail.com>
2022-12-01 22:00:19 +0500

    v4

---
+++ src/component/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand">Navbar</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
